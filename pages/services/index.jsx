73d9645caf312ba6980dbbe2ac3ac4e4e2a971c3
Alfredo Allan <allan-mecanico@outlook.com>
2025-06-29 18:29:31 -0300

    Ajustando rolagem na pagina services

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -5,16 +5,15 @@ import Circles from "../../components/Circles";
 import ServiceSlider from "../../components/ServiceSlider";
 import { fadeIn } from "../../variants";
 
-
 export const serviceData = [];
 
 const Services = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
+    <div className="min-h-screen bg-primary/30 py-32 text-center xl:text-left overflow-y-auto xl:overflow-hidden xl:h-screen">
       <Circles />
-      <div className="container mx-auto">
+      <div className="container mx-auto h-full flex flex-col justify-center">
         <div className="flex flex-col xl:flex-row gap-x-8">
-          {/* text */}
+          {/* texto */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
             <motion.h2
               variants={fadeIn("up", 0.2)}
@@ -33,12 +32,9 @@ const Services = () => {
               className="text-sm md:text-lg max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
             >
               Sou desenvolvedor <span className="text-accent">full stack</span> com experiência em criação de sistemas web e mobile.
-              No front-end, atuo com  <span className="text-accent">React, Next.js, Tailwind CSS, React Native e Expo Go</span>,
-              desenvolvendo interfaces responsivas e funcionais. No back-end,
-              utilizo <span className="text-accent">Python</span> com <span className="text-accent">Flask</span> e bancos de dados <span className="text-accent">SQL,</span> com deploys funcionais via <span className="text-accent">PythonAnywhere.</span>
+              No front-end, atuo com <span className="text-accent">React, Next.js, Tailwind CSS, React Native e Expo Go</span>,
+              desenvolvendo interfaces responsivas e funcionais. No back-end, utilizo <span className="text-accent">Python</span> com <span className="text-accent">Flask</span> e bancos de dados <span className="text-accent">SQL</span>, com deploys via <span className="text-accent">PythonAnywhere</span>.
               Integro ferramentas como <span className="text-accent">GitHub, Vercel</span> e soluções de IA para automações e melhorias contínuas.
-              Busco criar aplicações funcionais,
-              com código limpo e foco em resolver problemas reais com agilidade e eficiência.
             </motion.p>
           </div>
 
